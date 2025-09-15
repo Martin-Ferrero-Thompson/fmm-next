42fd0b49a25a16699a8f73ef2701da987f5d6093
Martin Ferrero-Thompson <martin.ferrero.thompson@gmail.com>
2025-09-15 08:56:09 +0200

    feat - footer - Format change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,8 +6,7 @@ export default function Footer() {
   return (
     <footer className="bg-gray-800 text-gray-400">
       <div className="container mx-auto px-4 py-6 text-center">
-        <p>&copy; {currentYear} Fleet Mucky Mayhem. All Rights Reserved.</p>
-        <p className="mt-1 text-xs">Version {version}</p>
+        <p>Copyright &copy; {currentYear} Fleet Mucky Mayhem | v{version} Designed & Built by The Spanish Mucky - All Rights Reserved.</p>
         <div className="mt-2">
           {/* Replace with your actual social media links */}
           <a href="#" className="hover:text-white mx-2">Facebook</a>
